1043a7a4bd0de2783413605b996ac02bf25db370
brun1nh00 <bruno.p.cardoso@hotmail.com>
2024-11-27 06:54:45 -0300

    add: exercise to practice "for"

---
+++ exercicios/src/estrutura_repetitiva/for_06.java
@@ -0,0 +1,21 @@
+package estrutura_repetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class for_06 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+        }
+    }
+        sc.close();
+    }
+}

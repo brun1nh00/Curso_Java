9d3799c668c9c462122e1b55729dd15c8a2c0e13
brun1nh00 <bruno.p.cardoso@hotmail.com>
2025-01-27 16:58:03 -0300

    alguns exercicios para praticar poo

--- exercicios/src/application/ProgramRectangle.java
+++ exercicios/src/application/ProgramRectangle.java
@@ -5,7 +5,7 @@ import entities.Rectangle;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Ex_Rectangle {
+public class ProgramRectangle {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
--- exercicios/src/entities/Employee.java
+++ exercicios/src/entities/Employee.java
@@ -11,5 +11,13 @@ public class Employee {
         return grossSalary - tax;
     }
 
-    public
+    public void increaseSalary (double percentage) {
+        grossSalary += grossSalary * percentage / 100;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", netSalary());
+    }
 }

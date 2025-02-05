c1556d9f3bb16ac99939ae822163820a77dc0f0f
brun1nh00 <bruno.p.cardoso@hotmail.com>
2025-02-05 06:14:10 -0300

    add: create ProductArray class to manage product data with encapsulation

---
+++ exercicios/src/entities/ProductArray.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class ProductArray {
+    private String name;
+    private double price;
+
+    public ProductArray() {
+    }
+
+    public ProductArray(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
